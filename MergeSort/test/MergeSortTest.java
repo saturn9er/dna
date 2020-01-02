b56c8bc4c805aa8bb4c922065a2e29f9fdbc1141
saturn9er <saturn9er@gmail.com>
2020-01-02 21:26:06 +0300

    Update unit tests
    Due to the fact that some methods became private, a few test became pointless. Also added a test that covers new functionaluty.

--- MergeSort/test/MergeSortTest.java
+++ MergeSort/test/MergeSortTest.java
@@ -6,22 +6,6 @@ import org.junit.Test;
 public class MergeSortTest {
 
     @Test
-    public void testMergeSingles() {
-        Comparable[] actual = MergeSort.merge(new Comparable[]{3}, new Comparable[]{8});
-        Comparable[] expected = {3, 8};
-
-        assertThat(actual, equalTo(expected));
-    }
-
-    @Test
-    public void testMergeMultiples() {
-        Comparable[] actual = MergeSort.merge(new Comparable[]{3, 4, 6}, new Comparable[]{8, 10, 42, 88});
-        Comparable[] expected = {3, 4, 6, 8, 10, 42, 88};
-
-        assertThat(actual, equalTo(expected));
-    }
-
-    @Test
     public void testSortIntegers() {
         Comparable[] actual = MergeSort.sort(new Comparable[]{4, 1, 6, 2, 0, 41, 23, 92, -22});
         Comparable[] expected = {-22, 0, 1, 2, 4, 6, 23, 41, 92};
@@ -33,6 +17,14 @@ public class MergeSortTest {
     public void testSortDoubles() {
         Comparable[] actual = MergeSort.sort(new Comparable[]{3.32, 0.1, 45.11345, 3.33, -10.2301, 0.000001});
         Comparable[] expected = {-10.2301, 0.000001, 0.1, 3.32, 3.33, 45.11345};
+
+        assertThat(actual, equalTo(expected));
+    }
+
+    @Test
+    public void testSortNotMatchingTypes() {
+        Comparable[] actual = MergeSort.sort(new Comparable[]{2.01, 10, 0.112, 8});
+        Comparable[] expected = {2.01, 10, 0.112, 8};
 
         assertThat(actual, equalTo(expected));
     }
