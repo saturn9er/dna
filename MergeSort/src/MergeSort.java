4f3815346b05cc99f89d2ce6d309abeaaf7e1b86
saturn9er <saturn9er@gmail.com>
2020-01-02 21:20:59 +0300

    Fix multiple types array sorting exception
    Now can handle the case when sort() method is passed with an array containing Comparable values of different types insted of throwing an exception.

--- MergeSort/src/MergeSort.java
+++ MergeSort/src/MergeSort.java
@@ -4,22 +4,35 @@ import java.util.Arrays;
  * The class implements an elegant merge sort algorithm,
  * which is based on divide and conquer algorithm design
  * paradigm based on multi-branched recursion. This algorithm
- * divides input array of Comparable in halves, calls itself
+ * divides input array of {@code Comparable} in halves, calls itself
  * for two halves and then merges the two sorted halves together.
  * This particular implementation uses top-down approach and
- * Comparable array (Comparable[]).
+ * {@code Comparable} array (Comparable[]).
  * @author Roman Romankov (saturn9er)
  */
 public class MergeSort {
 
+    public static Comparable[] sort(Comparable[] array) {
+        Comparable[] sorted = array;
+
+        try {
+            checkTypes(array);
+            sorted = mergesort(array);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return sorted;
+    }
+
     /**
      * Sorts elements of two arrays in ascending order by comparing values in both arrays,
      * then concatenates them in a single array, and then returns it.
-     * @param left first (or left) array of Comparable (or part) to undergo sorting and merging
-     * @param right last (or right) array of Comparable (or part) to undergo sorting and merging
-     * @return a sorted array of Comparable
+     * @param left first half of array of {@code Comparable} to undergo sorting and merging
+     * @param right second half of array of {@code Comparable} to undergo sorting and merging
+     * @return a sorted array of {@code Comparable}
      */
-    static Comparable[] merge(Comparable[] left, Comparable[] right) {
+    private static Comparable[] merge(Comparable[] left, Comparable[] right) {
         Comparable[] merged = new Comparable[left.length + right.length];
 
         // indices
@@ -55,19 +68,45 @@ public class MergeSort {
     /**
      * Recursively splits an array in halves then evokes merge() to do the sorting
      * and merging on split parts, then returns an array sorted in ascending order.
-     * @param array unsorted array of Comparable
-     * @return sorted array of Comparable
+     * @param array unsorted array of {@code Comparable}
+     * @return sorted array of {@code Comparable}
      */
-    public static Comparable[] sort(Comparable[] array) {
+    private static Comparable[] mergesort(Comparable[] array) {
         int length = array.length;
         Comparable[] result = array;
 
         if (length > 1) {
             int median = array.length >> 1;
-            Comparable[] left = sort(Arrays.copyOfRange(array, 0, median));
-            Comparable[] right = sort(Arrays.copyOfRange(array, median, length));
+            Comparable[] left = mergesort(Arrays.copyOfRange(array, 0, median));
+            Comparable[] right = mergesort(Arrays.copyOfRange(array, median, length));
             result = merge(left, right);
         }
         return result;
+    }
+
+    /**
+     * @return The {@code Class} object that represents the runtime
+     *         class of the first element of a given array.
+     */
+    static Class getFirstElementClass(Comparable[] array) {
+        Comparable first = array[0];
+
+        return first.getClass();
+    }
+
+    /**
+     * Checks that all elements of a given array are of the same type as the first element.
+     */
+    public static void checkTypes(Comparable[] array) {
+        Class firstElementClass = getFirstElementClass(array);
+
+        if(array.length > 0) {
+            for (Comparable element : array) {
+                if(!(element.getClass().equals(firstElementClass))) {
+                    throw new IllegalArgumentException("Elements type doesn't match. " +
+                            "The type of all elements in an array must be the same.");
+                }
+            }
+        }
     }
 }
